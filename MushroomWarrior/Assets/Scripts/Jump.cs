a2276b7629d875ad7b53b3b1658fc7009f837173
Aaron Thompson <amt52394@yahoo.com>
2019-03-15 20:24:39 -0700

    initial commit

---
+++ MushroomWarrior/Assets/Scripts/Jump.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Jump : MonoBehaviour{
+
+	public Vector3 jumpVector;
+	public float jumpForce = 2.0f;
+
+	public bool isGrounded;
+	Rigidbody2D rgbd;
+	void Start(){
+		rgbd = GetComponent<Rigidbody2D>();
+		jumpVector = new Vector3(0.0f, 2.0f, 0.0f);
+	}
+
+	void OnCollisionStay(){
+			isGrounded = true;
+		}
+
+
+	// Button , tap screen to jump
+	public Button jumpButton;
+
+	//Function for jump button
+	/*public void JumpButton(){
+		if(isGrounded){
+			rgbd.AddForce(jumpVector * jumpForce, ForceMode2D.Impulse);
+			isGrounded = false;
+		}
+	}*/
+
+	void Update(){
+		/*if(Input.touchCount == 1){
+			if(isGrounded){
+				rgbd.AddForce(jumpVector * jumpForce, ForceMode2D.Impulse);
+				isGrounded = false;
+			}
+		}*/
+		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			if(isGrounded){
+				rgbd.AddForce(jumpVector * jumpForce, ForceMode2D.Impulse);
+				isGrounded = false;
+			}
+		}
+	}
+
+	/*
+	void OnCollisionEnter(Collision collision){
+		if(collision.gameObject.CompareTag("Ground")){
+			isGrounded = true;
+		}
+	}
+
+	void OnCollisionExit(Collision collision){
+		if(collision.gameObject.CompareTag("Ground")){
+			isGrounded = false;
+		}
+	}
+	*/
+}
---
+++ MushroomWarrior/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour{
+
+	// This creates a button in Unity for attaching the UI button to. This starts the game
+	public Button startButton;
+
+	// This button brings you back to the main menu
+	public Button backToMainMenuButton;
+
+	// This button exits the game
+	public Button exitButton;
+
+	// This function makes it do something, appears on main menu
+	public void startPress(){
+		// 0 is Main Menu, 1 is Game Scene
+		SceneManager.LoadScene(1);
+	}
+
+	// This function takes you to main menu, appears in game
+	public void backToMainMenuPress(){
+		SceneManager.LoadScene(0);
+	}
+
+	// This function exits the game, appears on main menu
+	public void exitPress(){
+		Debug.Log("Exiting game...");
+		Application.Quit();
+	}
+}
