1464747e5b55d8dd856733d8ef1173a04060d344
Sean Spink <43582537+PixelDotPixel@users.noreply.github.com>
2019-03-23 18:20:19 -0700

    Fixed Jump Function (#1)
    * Uploading Updates ProjectSettings
    
    * Updating temp, logs, and packages
    
    * Updating Assets
    
    * Updating .meta's
    
    * Fixed Jump function
    
    Jump functionality now works, changed the name of "Ground" to "ground" causing the jump.cs to be able to find the ground tag and detect collision.

--- MushroomWarrior/Assets/Scripts/Jump.cs
+++ MushroomWarrior/Assets/Scripts/Jump.cs
@@ -10,14 +10,15 @@ public class Jump : MonoBehaviour{
 
 	public bool isGrounded;
 	Rigidbody2D rgbd;
+
 	void Start(){
 		rgbd = GetComponent<Rigidbody2D>();
 		jumpVector = new Vector3(0.0f, 2.0f, 0.0f);
 	}
-
+    /* 
 	void OnCollisionStay(){
 			isGrounded = true;
-		}
+	}*/
 
 
 	// Button , tap screen to jump
@@ -32,29 +33,25 @@ public class Jump : MonoBehaviour{
 	}*/
 
 	void Update(){
-		/*if(Input.touchCount == 1){
-			if(isGrounded){
-				rgbd.AddForce(jumpVector * jumpForce, ForceMode2D.Impulse);
-				isGrounded = false;
-			}
-		}*/
 		if(Input.GetKeyDown(KeyCode.UpArrow)){
-			if(isGrounded){
-				rgbd.AddForce(jumpVector * jumpForce, ForceMode2D.Impulse);
-				isGrounded = false;
+			if(isGrounded){														//If the player is not in the middle of a jump
+				rgbd.AddForce(jumpVector * jumpForce, ForceMode2D.Impulse);		//Make the player jump with 2.0f force in the Y direction
+				isGrounded = false;												//Dont allow the player to double jump
 			}
-		}
+		} 
 	}
 
+	
+	void OnCollisionEnter2D(Collision2D collision){
+		//Debug.Log("This is a collision");
+		//Debug.Log("isGrounded == " + isGrounded);
+		if(collision.gameObject.CompareTag("ground")){							//check to see if the contact was made with ground
+			isGrounded = true;													//If contact was made allow the player to jump again
+		}
+	}
 	/*
-	void OnCollisionEnter(Collision collision){
-		if(collision.gameObject.CompareTag("Ground")){
-			isGrounded = true;
-		}
-	}
-
 	void OnCollisionExit(Collision collision){
-		if(collision.gameObject.CompareTag("Ground")){
+		if(collision.gameObject.CompareTag("ground2")){
 			isGrounded = false;
 		}
 	}
