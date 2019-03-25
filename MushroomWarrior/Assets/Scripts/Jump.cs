f97ee1aa77b8d683a272886f1f5257e990542c0a
Aaron Thompson <amt52394@yahoo.com>
2019-03-24 18:02:15 -0700

    Added Jump Counter to jump script

--- MushroomWarrior/Assets/Scripts/Jump.cs
+++ MushroomWarrior/Assets/Scripts/Jump.cs
@@ -7,53 +7,37 @@ public class Jump : MonoBehaviour{
 
 	public Vector3 jumpVector;
 	public float jumpForce = 2.0f;
-
 	public bool isGrounded;
 	Rigidbody2D rgbd;
+    public Button jumpButton;
 
-	void Start(){
-		rgbd = GetComponent<Rigidbody2D>();
+    public int jumpCounter = 0;
+
+    public void JumpButton(){
+        Debug.Log("Jump button pressed");
+        rgbd.AddForce(jumpVector * jumpForce, ForceMode2D.Impulse);
+        isGrounded = false;
+    }
+
+    void Start(){
+        rgbd = GetComponent<Rigidbody2D>();
 		jumpVector = new Vector3(0.0f, 2.0f, 0.0f);
 	}
-    /* 
-	void OnCollisionStay(){
-			isGrounded = true;
-	}*/
-
-
-	// Button , tap screen to jump
-	public Button jumpButton;
-
-	//Function for jump button
-	public void JumpButton(){
-		if(isGrounded){
-			rgbd.AddForce(jumpVector * jumpForce, ForceMode2D.Impulse);
-			isGrounded = false;
-		}
-	}
-
+    
 	void Update(){
 		if(Input.GetKeyDown(KeyCode.UpArrow)){
 			if(isGrounded){														//If the player is not in the middle of a jump
 				rgbd.AddForce(jumpVector * jumpForce, ForceMode2D.Impulse);		//Make the player jump with 2.0f force in the Y direction
 				isGrounded = false;												//Dont allow the player to double jump
+                jumpCounter++;
+                Debug.Log("You have jumped " + jumpCounter + " times");
 			}
-		} 
+		}
 	}
-
-	
+    
 	void OnCollisionEnter2D(Collision2D collision){
-		//Debug.Log("This is a collision");
-		//Debug.Log("isGrounded == " + isGrounded);
 		if(collision.gameObject.CompareTag("ground")){							//check to see if the contact was made with ground
 			isGrounded = true;													//If contact was made allow the player to jump again
 		}
 	}
-	/*
-	void OnCollisionExit(Collision collision){
-		if(collision.gameObject.CompareTag("ground2")){
-			isGrounded = false;
-		}
-	}
-	*/
 }
