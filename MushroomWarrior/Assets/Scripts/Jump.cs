5583909e58b1acc926a528ab2cfe634f681377d9
Sean Spink <43582537+PixelDotPixel@users.noreply.github.com>
2019-03-26 16:05:19 -0700

    Added variablity to jump
    Jump feels a little more natural (to a game) with variable jumping heights based off button press duration. This update brings more user control to jump height.

--- MushroomWarrior/Assets/Scripts/Jump.cs
+++ MushroomWarrior/Assets/Scripts/Jump.cs
@@ -4,59 +4,41 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Jump : MonoBehaviour{
-
+	private int maxJumpHeight = 15;
 	public Vector3 jumpVector;
-	public float jumpForce = 2.0f;
-
+	public float jumpForce = 4.0f;
 	public bool isGrounded;
 	Rigidbody2D rgbd;
-	void Start(){
-		rgbd = GetComponent<Rigidbody2D>();
-		jumpVector = new Vector3(0.0f, 2.0f, 0.0f);
+    public Button jumpButton;
+    public int jumpCounter = 0;
+
+    public void JumpButton(){
+        Debug.Log("Jump button pressed");
+        rgbd.AddForce(jumpVector * jumpForce, ForceMode2D.Impulse);
+        isGrounded = false;
+    }
+
+    void Start(){
+        rgbd = GetComponent<Rigidbody2D>();
+		jumpVector = new Vector3(0.0f, 0.25f, 0.0f);
 	}
-
-	void OnCollisionStay(){
-			isGrounded = true;
-		}
-
-
-	// Button , tap screen to jump
-	public Button jumpButton;
-
-	//Function for jump button
-	/*public void JumpButton(){
-		if(isGrounded){
-			rgbd.AddForce(jumpVector * jumpForce, ForceMode2D.Impulse);
-			isGrounded = false;
-		}
-	}*/
-
+    
 	void Update(){
-		/*if(Input.touchCount == 1){
-			if(isGrounded){
-				rgbd.AddForce(jumpVector * jumpForce, ForceMode2D.Impulse);
-				isGrounded = false;
-			}
-		}*/
-		if(Input.GetKeyDown(KeyCode.UpArrow)){
-			if(isGrounded){
-				rgbd.AddForce(jumpVector * jumpForce, ForceMode2D.Impulse);
-				isGrounded = false;
+		if(Input.GetKey(KeyCode.UpArrow)){
+			if(isGrounded){														//If the player is not in the middle of a jump
+				rgbd.AddForce(jumpVector * jumpForce, ForceMode2D.Impulse);		//Make the player jump with 2.0f force in the Y direction
+				jumpCounter++;
+				if(jumpCounter == maxJumpHeight)
+					isGrounded = false;												//Dont allow the player to double jump
+                Debug.Log("You have jumped " + jumpCounter + " times");
 			}
 		}
 	}
-
-	/*
-	void OnCollisionEnter(Collision collision){
-		if(collision.gameObject.CompareTag("Ground")){
-			isGrounded = true;
+    
+	void OnCollisionEnter2D(Collision2D collision){
+		if(collision.gameObject.CompareTag("ground")){							//check to see if the contact was made with ground
+			isGrounded = true;													//If contact was made allow the player to jump again
+			jumpCounter = 0;
 		}
 	}
-
-	void OnCollisionExit(Collision collision){
-		if(collision.gameObject.CompareTag("Ground")){
-			isGrounded = false;
-		}
-	}
-	*/
 }
