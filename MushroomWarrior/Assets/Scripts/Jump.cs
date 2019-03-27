d8171e1cd6ce847d09939b1c037cbdc70fdb6148
Sean Spink <43582537+PixelDotPixel@users.noreply.github.com>
2019-03-26 21:30:11 -0700

    Removes the ability to double jump

--- MushroomWarrior/Assets/Scripts/Jump.cs
+++ MushroomWarrior/Assets/Scripts/Jump.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Jump : MonoBehaviour{
-	private int maxJumpHeight = 15;
+	public int maxJumpHeight = 15;
+	public int jumpCounter = 0;
+	public int jumpFrames = 0;
+	public int heldFrames = 0;
 	public Vector3 jumpVector;
 	public float jumpForce = 4.0f;
 	public bool isGrounded;
 	Rigidbody2D rgbd;
-    public Button jumpButton;
-    public int jumpCounter = 0;
+
 
     public void JumpButton(){
         Debug.Log("Jump button pressed");
@@ -24,21 +26,29 @@ public class Jump : MonoBehaviour{
 	}
     
 	void Update(){
+		
 		if(Input.GetKey(KeyCode.UpArrow)){
-			if(isGrounded){														//If the player is not in the middle of a jump
-				rgbd.AddForce(jumpVector * jumpForce, ForceMode2D.Impulse);		//Make the player jump with 2.0f force in the Y direction
-				jumpCounter++;
-				if(jumpCounter == maxJumpHeight)
-					isGrounded = false;												//Dont allow the player to double jump
-                Debug.Log("You have jumped " + jumpCounter + " times");
-			}
+			jumpLogic();
+		} else {
+			heldFrames = 0;
 		}
 	}
     
+	void jumpLogic(){
+		if(isGrounded && (heldFrames == jumpFrames)){															//If the player is not in the middle of a jump
+			rgbd.AddForce(jumpVector * jumpForce, ForceMode2D.Impulse);			//Make the player jump with 2.0f force in the Y direction
+			jumpFrames++;
+			heldFrames++;
+			if(jumpFrames == maxJumpHeight)
+				isGrounded = false;												//Dont allow the player to double jump
+            Debug.Log("You have jumped " + jumpCounter + " times");
+		}
+		
+	}
 	void OnCollisionEnter2D(Collision2D collision){
 		if(collision.gameObject.CompareTag("ground")){							//check to see if the contact was made with ground
 			isGrounded = true;													//If contact was made allow the player to jump again
-			jumpCounter = 0;
+			jumpFrames = 0;
 		}
 	}
 }
